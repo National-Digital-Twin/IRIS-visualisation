9b0ce6614f198cb5680527fdccb87cfe1ac3d405
CSturzaker <craig.sturzaker@arup.com>
2023-11-08 12:21:42 +0000

    feat(map): add map component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { RouterOutlet } from '@angular/router';
 
 import '@arc-web/components';
 
+import { MapComponent } from './map/map.component';
+
 @Component({
   selector: 'c477-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
