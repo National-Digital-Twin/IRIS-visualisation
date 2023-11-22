3784ba766f3498a2c2bc8ca6889e194346152d31
CSturzaker <craig.sturzaker@arup.com>
2023-11-22 18:38:43 +0000

    refactor(shell): move content into shell component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 import '@arc-web/components';
-
-import { MapComponent } from './components/map/map.component';
 
 @Component({
   selector: 'c477-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MapComponent],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {
-  title = 'C477 Visualisation';
-}
+export class AppComponent {}
