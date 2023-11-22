0ae8421aafb2c7bafe9400b7c5092ebf9a0614a9
CSturzaker <craig.sturzaker@arup.com>
2023-11-22 23:18:57 +0000

    feat(shell component): add shell container
    add shell container component to act as parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 import '@arc-web/components';
 
 @Component({
   selector: 'c477-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/containers/shell/shell.component.ts
+++ src/app/containers/shell/shell.component.ts
@@ -1,7 +1,9 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Polygon } from 'geojson';
 
 import { MapComponent } from '@components/map/map.component';
+import { SpatialQueryService } from '@core/services/spatial-query.service';
 
 @Component({
   selector: 'c477-shell',
@@ -12,5 +14,10 @@ import { MapComponent } from '@components/map/map.component';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ShellComponent {
+  spatialQueryService = inject(SpatialQueryService);
   title = 'C477 Visualisation';
+
+  setSearchArea(searchArea: GeoJSON.Feature<Polygon>) {
+    this.spatialQueryService.selectBuildings(searchArea);
+  }
 }
