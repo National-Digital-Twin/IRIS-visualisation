5e3c3d8eb032d2a46a184b3ec5b07149c180f404
CSturzaker <craig.sturzaker@arup.com>
2023-11-23 21:11:22 +0000

    feat(selected building): store TOID of selected building

--- src/app/core/services/spatial-query.service.ts
+++ src/app/core/services/spatial-query.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 
 import bbox from '@turf/bbox';
 import intersect from '@turf/intersect';
@@ -12,6 +12,12 @@ import { Expression } from 'mapbox-gl';
 })
 export class SpatialQueryService {
   private mapService = inject(MapService);
+
+  selectedBuildingTOID = signal<string | undefined>(undefined);
+
+  setSelectedTOID(toid: string) {
+    this.selectedBuildingTOID.set(toid);
+  }
 
   selectBuilding(toid: string) {
     this.mapService.filterMapLayer(
