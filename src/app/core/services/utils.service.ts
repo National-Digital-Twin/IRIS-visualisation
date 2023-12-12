4302494d2b31cfac65f44967abcbdd2fdf4daf95
CSturzaker <craig.sturzaker@arup.com>
2023-12-12 10:49:13 +0000

    refactor(utils): move expression creation into a utils service

---
+++ src/app/core/services/utils.service.ts
@@ -0,0 +1,182 @@
+import { Injectable, inject, signal } from '@angular/core';
+
+import { Expression } from 'mapbox-gl';
+import booleanWithin from '@turf/boolean-within';
+import { Polygon } from 'geojson';
+
+import { DataService } from './data.service';
+import { MapService } from './map.service';
+import { SpatialQueryService } from './spatial-query.service';
+
+import { RUNTIME_CONFIGURATION } from '@core/tokens/runtime-configuration.token';
+
+import { ToidMap } from '@core/models/toid.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilService {
+  private dataService = inject(DataService);
+  private mapService = inject(MapService);
+  private spatialQueryService = inject(SpatialQueryService);
+  private runtimeConfig = inject(RUNTIME_CONFIGURATION);
+
+  private epcColours = this.runtimeConfig.epcColours;
+
+  currentMapViewExpression = signal<Expression | undefined>(undefined);
+
+  /**
+   * Create an array of building TOIDS and colours from buildings
+   * @param addresses filtered addresses within map bounds
+   * @returns MapboxGLJS expression
+   */
+  createBuildingColourFilter() {
+    const allToids = this.dataService.toids();
+    const epcs = this.dataService.epcs();
+    const spatialFilter = this.spatialQueryService.spatialFilterBounds();
+    const toids = this.filterTOIDSWithinBounds(allToids!, spatialFilter);
+    // if there is a spatial filter get the UPRNs within the filter area
+    // and set in signal to get data from IA to display in filter results
+    if (spatialFilter) {
+      const uprns = this.getUPRNsWithSpatialFilter(toids);
+      uprns.length === 1
+        ? this.dataService.setSelectedUPRN(uprns[0])
+        : this.dataService.setSelectedUPRNs(uprns);
+    }
+    const matchExpression: Expression = ['match', ['get', 'TOID']];
+    // iterate through toid object and get toid (as key)
+    Object.keys(toids).forEach((key: string) => {
+      const toid = key;
+      // get the uprns for the corresponding toid
+      const uprns: number[] = toids[key];
+      /* One UPRN for a TOID */
+      if (uprns.length === 1) {
+        const epc = epcs![uprns[0]];
+        if (epc) {
+          const colour = this.getEPCColour(epc['epc']);
+          matchExpression.push(toid, colour);
+        } else {
+          matchExpression.push(toid, this.epcColours['default']);
+        }
+      } else if (uprns.length > 1) {
+        /**
+         * Multiple UPRNs for a single TOID.
+         *
+         * Get the EPC value for each
+         * UPRN and add to array
+         */
+        const buildingEPCs: string[] = [];
+        uprns.forEach(uprn => {
+          const epc = epcs![uprn];
+          if (epc) {
+            buildingEPCs.push(epc['epc']);
+          }
+        });
+        /**
+         * If there are no EPCs for any of the
+         * building UPRNs, add the default colour
+         */
+        if (buildingEPCs.length === 0) {
+          matchExpression.push(toid, this.epcColours['default']);
+        } else {
+          /**
+           * If there are mulitple EPCs, get the mean value
+           */
+          const meanEPC = this.getMeanEPCValue(buildingEPCs);
+
+          const buildingColor = this.getEPCColour(meanEPC);
+          matchExpression.push(toid, buildingColor);
+        }
+      }
+    });
+    matchExpression.push(this.epcColours['default']);
+    return matchExpression;
+  }
+
+  /**
+   *
+   * @param epcRatings Array of EPC ratings
+   * @returns The mean EPC rating
+   */
+  getMeanEPCValue(epcRatings: string[]) {
+    let meanEPC = '';
+    // assign a weighting to the EPC ratings
+    const weightings: { [key: string]: number } = {
+      A: 1,
+      B: 2,
+      C: 3,
+      D: 4,
+      E: 5,
+      F: 6,
+      G: 7,
+    };
+    const scores: number[] = [];
+    // get the weighting for each epc value
+    epcRatings.forEach(val => scores.push(weightings[val]));
+    const sum = scores.reduce((a, c) => a + c, 0);
+    const mean = sum / scores.length;
+    Object.keys(weightings).forEach((epc: string) => {
+      // find the corresponding weighting for the mean
+      if (Math.floor(mean) === weightings[epc]) {
+        meanEPC = epc;
+      }
+    });
+    return meanEPC;
+  }
+
+  getEPCColour(SAPBand: string) {
+    const color = SAPBand
+      ? this.epcColours[SAPBand]
+      : this.epcColours['default'];
+    return color;
+  }
+
+  filterTOIDSWithinBounds(toids: ToidMap, spatialQueryBounds?: number[]) {
+    // if there is a spatial filter get features
+    // within it's bounding box, else get features
+    // within map extent
+    const currentFeatures = spatialQueryBounds
+      ? this.mapService.queryFeaturesByGeom(spatialQueryBounds)
+      : this.mapService.queryFeatures();
+
+    // check if there is a user drawn spatial filter
+    const spatialFilter = spatialQueryBounds
+      ? this.spatialQueryService.spatialFilterGeom()
+      : undefined;
+    const filteredToids: ToidMap = {};
+    currentFeatures
+      .filter(feature =>
+        // if there is a spatial filter
+        // remove any features outside of
+        // the filter geometry
+        spatialFilter
+          ? booleanWithin(
+              feature.geometry as Polygon,
+              spatialFilter?.geometry as Polygon
+            )
+          : feature
+      )
+      .forEach(feature => {
+        const toid = toids[feature.properties!.TOID];
+        if (toid) {
+          filteredToids[feature.properties!.TOID] = toid;
+        }
+      });
+    return filteredToids;
+  }
+
+  /**
+   * Get the UPRNs for the buildings within the spatial
+   * filter
+   * @param filteredToids toids with spatial filter area
+   * @returns array of uprns
+   */
+  getUPRNsWithSpatialFilter(filteredToids: ToidMap): number[] {
+    let filteredUPRNs: number[] = [];
+    Object.keys(filteredToids).forEach((toid: string) => {
+      const uprns = this.dataService.getBuildingUPRNs(toid);
+      filteredUPRNs = filteredUPRNs.concat(uprns);
+    });
+    return filteredUPRNs;
+  }
+}
