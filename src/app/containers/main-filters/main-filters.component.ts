4a18a58f14fb14d140cd99f6943897a462ea967b
CSturzaker <craig.sturzaker@arup.com>
2023-12-28 16:28:19 +0000

    feat(main filters): set selected values if any exist

--- src/app/containers/main-filters/main-filters.component.ts
+++ src/app/containers/main-filters/main-filters.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LabelComponent } from '@components/label/label.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { FilterPanelComponent } from '@components/filter-panel/filter-panel.component';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+
+import { FilterPanelComponent } from '@containers/filter-panel/filter-panel.component';
+import { LabelComponent } from '@components/label/label.component';
+
+import { FilterService } from '@core/services/filter.service';
+
 import { EPCRating, PropertyType } from '@core/enums';
 
 @Component({
@@ -26,10 +31,15 @@ import { EPCRating, PropertyType } from '@core/enums';
   styleUrl: './main-filters.component.css',
 })
 export class MainFiltersComponent {
+  private filterService = inject(FilterService);
+
+  filterProps = this.filterService.filters;
+
   epcRatings: { [key: string]: string } = EPCRating;
-  buildingTypes: { [key: string]: string } = PropertyType;
+  propertyTypes: { [key: string]: string } = PropertyType;
 
   constructor(public dialog: MatDialog) {}
+
   getKeys(options: { [key: string]: string }) {
     return Object.keys(options);
   }
@@ -38,5 +48,13 @@ export class MainFiltersComponent {
     this.dialog.open(FilterPanelComponent, {
       panelClass: 'filter-panel',
     });
+  }
+
+  propertyTypeChange(e: MatSelectChange) {
+    console.log(e);
+  }
+
+  ratingChange(e: MatSelectChange) {
+    console.log(e);
   }
 }
