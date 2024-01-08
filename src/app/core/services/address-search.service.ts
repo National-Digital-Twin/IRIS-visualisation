e6ad4866aa04d6cd64a85ee84476f773a62c72b8
Dom Egginton <dominic.egginton@gmail.com>
2024-01-08 00:07:57 +0000

    feat(app): handle http errors (#54)
    This refactors the `ExceptionService` to implement the `ErrorHandler`
    interface and registers it as the application-wide error handler. The
    `ExceptionService` now handles all errors that are thrown or not handled
    by other error handlers. Errors are currently handled by logging them to
    the console. We may want to send errors to a monitoring service here and
    open a snackbar or dialog for appropriate errors messages to inform the
    user.
    
    An new method `ExceptionService.handleHttpError` that returns an
    `OperatorFunction` piping its source observable through the `catchError`
    allows for handling of `HttpErrorResponses`.
    
    A new interceptor `HandleHttpErrorInterceptor` has been created and
    registered as a multi `HTTP_INTERCEPTORS` provider. This interceptor
    pipes the source http event stream through the operator function
    returned from the `ExceptionService.handleHttpError` method. This allows
    for all http errors to be handled without having to add the `catchError`
    operator to every http request stream.

--- src/app/core/services/address-search.service.ts
+++ src/app/core/services/address-search.service.ts
@@ -1,13 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RUNTIME_CONFIGURATION } from '@core/tokens/runtime-configuration.token';
-import { Observable, map, catchError } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {
   AddressSearchData,
   AddressSearchResponse,
 } from '@core/models/address-search-results.model';
-import { ExceptionService } from './exception.service';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +14,6 @@ import { ExceptionService } from './exception.service';
 export class AddressSearchService {
   private readonly http: HttpClient = inject(HttpClient);
   private readonly runtimeConfiguration = inject(RUNTIME_CONFIGURATION);
-  private readonly exceptionService = inject(ExceptionService);
 
   constructor() {}
 
@@ -28,9 +26,6 @@ export class AddressSearchService {
           queryString
         )}&maxresults=10&output_srs=EPSG:4326&key=${environment.os.apiKey}`
       )
-      .pipe(
-        map((res: AddressSearchResponse) => res.results.map(r => r.DPA)),
-        catchError(this.exceptionService.handleError)
-      );
+      .pipe(map((res: AddressSearchResponse) => res.results.map(r => r.DPA)));
   }
 }
--- src/app/core/services/exception.service.ts
+++ src/app/core/services/exception.service.ts
@@ -1,25 +1,36 @@
-import { Injectable } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Injectable, ErrorHandler } from '@angular/core';
+import { HttpErrorResponse, HttpEvent } from '@angular/common/http';
+import { OperatorFunction, catchError, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ExceptionService {
+@Injectable({ providedIn: 'root' })
+export class ExceptionService implements ErrorHandler {
   /**
-   * Error handler
-   * @param err Http error response
-   * @returns error
+   * Handle Error.
+   *
+   * Handle errors that occur within the application.
+   * Log the error to the console.
    */
-  handleError(err: HttpErrorResponse): Observable<never> {
-    let errorMessage = '';
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-    }
-    console.error(errorMessage);
-    return throwError(() => new Error(errorMessage));
+  handleError(error: Error): void {
+    console.error(error);
+  }
+
+  /**
+   * Handle Http Error.
+   *
+   * Handles errors that occure within HTTP requests streams.
+   */
+  handleHttpError<T>(): OperatorFunction<HttpEvent<T>, HttpEvent<T>> {
+    return request$ =>
+      request$.pipe(
+        catchError((httpErrorResponse: HttpErrorResponse) => {
+          const errorMessage =
+            httpErrorResponse.error instanceof ErrorEvent
+              ? `A client-side or network error occurred: ${httpErrorResponse.error.message}`
+              : `Server returned code: ${httpErrorResponse.status}, error message is: ${httpErrorResponse.message}`;
+          const error = new Error(errorMessage);
+          this.handleError(error);
+          return throwError(() => new Error(errorMessage));
+        })
+      );
   }
 }
