a30115f9b5bd7f3565c9fc53ffbb079a89da52ab
Dom Egginton <dominic.egginton@gmail.com>
2024-01-16 14:16:20 +0000

    fix(app): fix flagging (#84)
    Fixes flagging dwellings by updating the `InvalidateFlagReason` enum
    keys to correspond to the API types and fixing a typo in the body of the
    invalidation request.
    
    This also removes `MockHttpClient`

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -38,14 +38,13 @@ import {
   NoEPCBuildingResponseModel,
 } from '@core/models/building-response.model';
 
-import { MockHttpClient } from '@core/services/mock-http-client.service';
 import { InvalidateFlagReason } from '@core/enums/invalidate-flag-reason';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private readonly http: HttpClient = inject(MockHttpClient);
+  private readonly http: HttpClient = inject(HttpClient);
   private readonly searchEndpoint: string = inject(SEARCH_ENDPOINT);
   private readonly writeBackEndpoint = inject(WRITE_BACK_ENDPOINT);
 
@@ -542,7 +541,7 @@ export class DataService {
       .post<NonNullable<BuildingModel['Flagged']>>(
         `${this.writeBackEndpoint}/invalidate-flag`,
         {
-          flatUri: building.Flagged,
+          flagUri: building.Flagged,
           assessmentTypeOverride: `ndt_ont:${key}`,
         },
         { withCredentials: true }
