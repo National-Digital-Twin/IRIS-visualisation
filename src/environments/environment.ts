9fe6c268d233e33b28f4f44f637f72b5daadc97b
nikan Negaresh <nikan.negaresh@informed.com>
2025-01-03 11:59:26 +0000

    Template edit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,16 @@
 export const environment = {
   production: true,
+  mapbox: {
+    apiKeyapiKey:
+      'pk.eyJ1Ijoia3VtYWlsLWthbXJhbiIsImEiOiJjbTRhNHVtN3QwM2RrMmxxejZkc2xscGlkIn0.zqTuuzQsPgdvvglcYo71oA',
+  },
   os: {
-    apiKey: 'undefined',
+    apiKey: 'owTuWWzxd5yFNplvzfGjLR0LaY8ZeU1x',
   },
   posthog: {
-    apiKey: 'undefined',
+    apiKey: 'phc_imnDMhAuY8OWgmNFIg6bvDI44JaQHuz0czzKF6jtcPP',
   },
   transparent_proxy: {
-    url: 'undefined',
+    url: 'https://iris.demo.ndtp.co.uk/transparent-proxy/',
   },
 };
