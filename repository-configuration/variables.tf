ce8ef8b89afb3aa520acc1737be474b8653d3fed
James Cruddas <james.cruddas@informed.com>
2025-01-30 19:11:35 +0000

    [DPAV-152] Add PR template

--- repository-configuration/variables.tf
+++ repository-configuration/variables.tf
@@ -16,8 +16,13 @@ variable "repository_description" {
   default     = "Private repo for IRIS, a 3D mapping tool visualising domestic properties and EPC data, developed for NDTP, by Arup. Built on Mapbox with JavaScript."
 }
 
-variable "jira_project_id" {
-  description = "Atlassian JIRA project identifier to be used for autolinking commit messages. This ID should match those which prefix issue identifiers, for example DPAV."
+variable "requirement_tracking_url_base" {
+  description = "Requirement tracking system URL base to be used for autolinking commit messages."
+  type        = string
+}
+
+variable "requirement_tracking_id" {
+  description = "Requirement identifier to be used for autolinking commit messages. This ID should match those which prefix issue identifiers, for example DPAV."
   type        = string
   default     = "DPAV"
 }
