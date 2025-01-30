ff64e45a17ba5496907ecd5da8c23be3e8c2f737
James Cruddas <james.cruddas@informed.com>
2025-01-30 13:44:23 +0000

    [DPAV-152] Add autolink solution

--- repository-configuration/variables.tf
+++ repository-configuration/variables.tf
@@ -15,3 +15,9 @@ variable "repository_description" {
   type        = string
   default     = "Private repo for IRIS, a 3D mapping tool visualising domestic properties and EPC data, developed for NDTP, by Arup. Built on Mapbox with JavaScript."
 }
+
+variable "jira_project_id" {
+  description = "Atlassian JIRA project identifier to be used for autolinking commit messages. This ID should match those which prefix issue identifiers, for example DPAV."
+  type        = string
+  default     = "DPAV"
+}
