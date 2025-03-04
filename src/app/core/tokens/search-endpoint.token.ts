43e03f2e77c44a9ee48dbb7daf85098be245b878
Andrew Cunliffe <andrew.cunliffe@informed.com>
2025-03-04 09:00:05 +0000

    chore(DPAV-559): move sparql into config

--- src/app/core/tokens/search-endpoint.token.ts
+++ src/app/core/tokens/search-endpoint.token.ts
@@ -1,20 +1,7 @@
 import { inject, InjectionToken } from '@angular/core';
 import { RUNTIME_CONFIGURATION } from '@core/tokens/runtime-configuration.token';
+import { environment } from '@environment';
 
-/**
- * Search Endpoint Factory.
- *
- * Factory function to create an search endpoint string.
- */
-function searchEndpointFactory(): string {
-    return `${inject(RUNTIME_CONFIGURATION).apiURL}/api/sparql/knowledge/query`;
-}
-
-/**
- * Search Endpoint.
- *
- * The search endpoint string.
- */
 export const SEARCH_ENDPOINT = new InjectionToken<string>('SEARCH_ENDPOINT', {
-    factory: searchEndpointFactory,
+    factory: (): string => `${inject(RUNTIME_CONFIGURATION).apiURL}${environment.sparql.path}`,
 });
