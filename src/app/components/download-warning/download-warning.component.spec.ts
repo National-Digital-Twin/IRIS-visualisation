835fe6837dc08a8bd8179e6a1dbb4344891f56b2
Andrew Cunliffe <andrew.cunliffe@informed.com>
2025-03-31 11:36:13 +0100

    feat(DPAV-219): additional unit tests added

---
+++ src/app/components/download-warning/download-warning.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DownloadDataWarningData } from '@core/models/download-data-warning.model';
+import { DownloadWarningComponent } from './download-warning.component';
+
+describe('DownloadWarningComponent', () => {
+    let component: DownloadWarningComponent;
+    let fixture: ComponentFixture<DownloadWarningComponent>;
+
+    const mockData: DownloadDataWarningData = {
+        addresses: ['123 Main St', '456 Elm St'],
+        addressCount: 2,
+    };
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [DownloadWarningComponent],
+            providers: [{ provide: MAT_DIALOG_DATA, useValue: mockData }],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DownloadWarningComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should expose the injected MAT_DIALOG_DATA via the data getter', () => {
+        expect(component.data).toEqual(mockData);
+    });
+});
