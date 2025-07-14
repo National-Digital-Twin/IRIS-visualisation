a3be5fb40469d90470648e26b7f96a41513f9f45
suarezmashd <Daniel.Suarez-Mash@informed.com>
2025-07-14 15:36:14 +0100

    feat(DPAV-1134): added fuel type UI change (#89)
    * feat(DPAV-1134): added fuel type UI change
    
    * feat(DPAV-1134): linting fixed
    
    * feat(OSPO): synchronise OSPO workflows
    
    ---------
    
    Co-authored-by: suarezmashd <207899929+suarezmashd@users.noreply.github.com>

---
+++ src/app/core/enums/fuel-type.ts
@@ -0,0 +1,20 @@
+export enum FuelType {
+    Anthracite = 'Anthracite',
+    // Biogas = 'Biogas',
+    Biomass = 'Biomass',
+    Coal = 'Coal',
+    Electricity = 'Electricity',
+    Fuel = 'Other',
+    LPG = 'Liquefied Petroleum Gas',
+    NaturalFuelGas = 'Natural Gas',
+    Oil = 'Oil',
+    // Other = 'Other',
+    SmokelessCoal = 'Smokeless Coal',
+    WoodChips = 'Wood Chips',
+    WoodLogs = 'Wood Logs',
+    WoodPellets = 'Wood Pellets',
+}
+
+// SPDX-License-Identifier: Apache-2.0
+// © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
+// and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
