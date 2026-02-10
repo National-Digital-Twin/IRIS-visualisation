bccd31c4cbf986a18fcb4d72b6f39fd4634f6d41
KumailKamranIS <kumail.kamran@informed.com>
2026-02-10 10:21:15 +0000

    feat(dpav-2417): added tests to the wdr section item

--- src/app/components/wind-driven-rain-section-item/wind-driven-rain-section-item.spec.ts
+++ src/app/components/wind-driven-rain-section-item/wind-driven-rain-section-item.spec.ts
@@ -1,5 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BuildingWindDrivenRainData } from '@core/services/climate-data.service';
 import { WindDrivenRainSectionItem } from './wind-driven-rain-section-item';
+
+function generateBuildingWindDrivenRainData(min: number, max: number): BuildingWindDrivenRainData {
+    return {
+        north_four_degrees_median: Math.random() * (max - min + 1) + min,
+        north_east_four_degrees_median: Math.random() * (max - min + 1) + min,
+        east_four_degrees_median: Math.random() * (max - min + 1) + min,
+        south_east_four_degrees_median: Math.random() * (max - min + 1) + min,
+        south_four_degrees_median: Math.random() * (max - min + 1) + min,
+        south_west_four_degrees_median: Math.random() * (max - min + 1) + min,
+        west_four_degrees_median: Math.random() * (max - min + 1) + min,
+        north_west_four_degrees_median: Math.random() * (max - min + 1) + min,
+        north_two_degrees_median: Math.random() * (max - min + 1) + min,
+        north_east_two_degrees_median: Math.random() * (max - min + 1) + min,
+        east_two_degrees_median: Math.random() * (max - min + 1) + min,
+        south_east_two_degrees_median: Math.random() * (max - min + 1) + min,
+        south_two_degrees_median: Math.random() * (max - min + 1) + min,
+        south_west_two_degrees_median: Math.random() * (max - min + 1) + min,
+        west_two_degrees_median: Math.random() * (max - min + 1) + min,
+        north_west_two_degrees_median: Math.random() * (max - min + 1) + min,
+    };
+}
 
 describe('WindDrivenRainSectionItem', () => {
     let component: WindDrivenRainSectionItem;
@@ -17,6 +40,36 @@ describe('WindDrivenRainSectionItem', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should not create the data table for the wind driven rain 4 degrees section if no data is provided', () => {
+        const fourDegressTableElement = fixture.debugElement.query(By.css('[data-testid="4-degress-table"]'));
+        expect(fourDegressTableElement).toBeFalsy();
+    });
+
+    it('should not create the data table for the wind driven rain 2 degrees section if no data is provided', () => {
+        const twoDegressTableElement = fixture.debugElement.query(By.css('[data-testid="2-degress-table"]'));
+        expect(twoDegressTableElement).toBeFalsy();
+    });
+
+    it('should create the data table for the wind driven rain 4 degrees section when data is provided', async () => {
+        component.data = generateBuildingWindDrivenRainData(0, 500);
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        const fourDegreesTableElement = fixture.debugElement.query(By.css('[data-testid="4-degrees-table"]')).nativeElement;
+        expect(fourDegreesTableElement).toBeTruthy();
+    });
+
+    it('should create the data table for the wind driven rain 2 degrees section when data is provided', async () => {
+        component.data = generateBuildingWindDrivenRainData(0, 500);
+
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        const twoDegreesTableElement = fixture.debugElement.query(By.css('[data-testid="2-degrees-table"]')).nativeElement;
+        expect(twoDegreesTableElement).toBeTruthy();
     });
 });
 
