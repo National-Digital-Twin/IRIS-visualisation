0a07889b2cf55dce0baa2d49a9237d2229c5e0d8
KumailKamranIS <kumail.kamran@informed.com>
2026-02-24 16:23:16 +0000

    feat(dpav-2541): added downloadable modal for the sunlight hours layer

--- src/app/components/hours-of-sunlight-section-item/hours-of-sunlight-section-item.ts
+++ src/app/components/hours-of-sunlight-section-item/hours-of-sunlight-section-item.ts
@@ -1,4 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DownloadableContentModal } from '@components/downloadable-content-modal/downloadable-content-modal';
 import { MoreInfoSectionItem } from '@components/more-info-section-item/more-info-section-item';
 import { BuildingSunlightHoursDataModel } from '@core/models/building.weather.data.model';
 
@@ -9,7 +12,28 @@ import { BuildingSunlightHoursDataModel } from '@core/models/building.weather.da
     styleUrl: './hours-of-sunlight-section-item.scss',
 })
 export class HoursOfSunlightSectionItem {
+    readonly #sanitizer = inject(DomSanitizer);
+    readonly #dialog = inject(MatDialog);
+
     public dataInput = input.required<BuildingSunlightHoursDataModel>();
+
+    public readonly downloadableWarningGuidanceMoreInfo = {
+        content: this.#sanitizer.bypassSecurityTrustHtml(`
+            <h2 style="font-weight: 550;">Hours of sunlight and retrofitting UK housing</h2>
+            <div style="display: flex; justify-content: center; padding: 30px;">
+                <p><em>More information coming soon</em></p>
+            </div>
+        `),
+    };
+
+    public onDownloadableWarningGuidanceMoreInfoClick(): void {
+        const dialogRef = this.#dialog.open(DownloadableContentModal, {
+            data: this.downloadableWarningGuidanceMoreInfo,
+            panelClass: 'section-item-warning-guidance-more-info-modal',
+        });
+
+        dialogRef.afterClosed().subscribe();
+    }
 }
 
 // SPDX-License-Identifier: Apache-2.0
